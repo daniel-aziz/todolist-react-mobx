047ff0125937dcc65a8cb182728dfa688b0b2829
daniel-aziz <91468647+daniel-aziz@users.noreply.github.com>
2022-03-01 21:11:05 +0200

    some changes to css

--- src/index.tsx
+++ src/index.tsx
@@ -2,25 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { TodoList } from './stores/TodoList';
-import { StoreProvider } from './helpers/store-provider';
+import {StoreProvider} from "./helpers/store-provider";
+import {TodoList} from "./stores/TodoList";
 
 const todoList = new TodoList([
-  'Should Starting Writing in React',
-  'Should Learn MobX',
-  'Should Watch Once Piece :)'
+    'Should Starting Writing in React',
+    'Should Learn MobX',
+    'Should Watch Once Piece :)'
 ]);
 
+//@ts-ignore - for debugging
+window.todoList = todoList
 
 ReactDOM.render(
-  <StoreProvider value={todoList}>
-    <App />
-  </StoreProvider>,
-  document.getElementById('root')
-);
+    <StoreProvider value={todoList}>
+        <App/>
+    </StoreProvider>
+    , document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
