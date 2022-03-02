4a83767633a67c47420007fe51cc03f607d43564
daniel-aziz <91468647+daniel-aziz@users.noreply.github.com>
2022-03-02 19:41:29 +0200

    update README

--- src/index.tsx
+++ src/index.tsx
@@ -20,7 +20,3 @@ ReactDOM.render(
     </StoreProvider>
     , document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
